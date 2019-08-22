75eaf9ba97b664d0491b3ab4f816125b95dd2c0e
tr4cefl0w <tr4cefl0w@protonmail.com>
2019-08-22 09:50:57 -0700

    modified:   source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,7 @@ release = '1.0.3'
 
 
 # -- General configuration ---------------------------------------------------
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
