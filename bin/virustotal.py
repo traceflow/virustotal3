1d1f9551dacd64b7f961893c3d36205279686b66
tr4cefl0w <tr4cefl0w@protonmail.com>
2020-02-27 20:52:44 -0500

    updated

--- bin/virustotal.py
+++ bin/virustotal.py
@@ -1,10 +1,11 @@
 import os
+import getpass
 import argparse
 
-try:
-    API_KEY = os.environ['VT_API']
-except KeyError:
-    print("Please set the API key in the VT_API environment variable")
+#try:
+#    API_KEY = os.environ['VT_API']
+#except KeyError:
+#    print("Please set the API key in the VT_API environment variable")
 
 # USAGE EXAMPLE
 # python3 virustotal.py info --ip
@@ -18,7 +19,7 @@ def relationships(ioc_type):
 
 # create the top-level parser
 parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers()
+subparsers = parser.add_subparsers(title='commands', dest='command')
 
 # create the parser for the "domain" command
 parser_info = subparsers.add_parser('info')
@@ -34,11 +35,21 @@ parser_relationships.add_argument('--ip')
 parser_relationships.add_argument('--file')
 #parser_relationships.set_defaults(func=bar)
 
-# parse the args and call whatever function was selected
-args = parser.parse_args()
-args.func(args)
-
 
 # parse the args and call whatever function was selected
 args = parser.parse_args()
-args.func(args)
+
+if args.command == 'info':
+    if args.domain:
+        pass
+    if args.ip:
+        pass
+    if args.file:
+        pass
+if args.command == 'relationships':
+    if args.domain:
+        pass
+    if args.ip:
+        pass
+    if args.file:
+        pass
